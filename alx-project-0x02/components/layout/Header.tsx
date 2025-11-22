89783bfe6d1043f97bb46427a6125213795ee3e1
Firezer27 <firezer.bizuwork27@gmail.com>
2025-11-22 17:28:56 +0300

    update the file

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,9 +1,19 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
     <header className="w-full bg-gray-800 text-white p-4">
-      <h1 className="text-xl font-semibold text-center">
-        ALX Project 0x02 - Setup
-      </h1>
+      <nav className="flex gap-6 justify-center">
+        <Link href="/" className="hover:text-blue-400">
+          Home
+        </Link>
+        <Link href="/home" className="hover:text-blue-400">
+          /home
+        </Link>
+        <Link href="/about" className="hover:text-blue-400">
+          /about
+        </Link>
+      </nav>
     </header>
   );
 };
