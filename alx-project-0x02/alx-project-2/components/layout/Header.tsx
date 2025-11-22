1e712f0c730f66e53e32afa1e651ccd543aeff55
Firezer27 <firezer.bizuwork27@gmail.com>
2025-11-22 17:21:38 +0300

    update index.tsx ,index.ts and other

---
+++ alx-project-0x02/alx-project-2/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="w-full bg-gray-800 text-white p-4">
+      <h1 className="text-xl font-semibold text-center">
+        ALX Project 0x02 - Setup
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
