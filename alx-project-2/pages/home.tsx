9d6119b9ab4e113b3cdddf8b54c9ec0ca990203c
Firezer27 <firezer.bizuwork27@gmail.com>
2025-11-22 05:43:07 +0300

    add component file and interface file and other

---
+++ alx-project-2/pages/home.tsx
@@ -0,0 +1,17 @@
+import Header from "../components/layout/Header"
+
+
+
+export default function HomePage() {
+  return (
+    <div>
+      <Header />
+      <main className="p-10">
+        <h1 className="text-3xl font-bold text-green-600">Home Page</h1>
+        <p className="mt-4 text-gray-700">
+          This is the Home page of the project.
+        </p>
+      </main>
+    </div>
+  );
+}
